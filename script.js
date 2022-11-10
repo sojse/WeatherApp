7a86c49a7e46e9d9aaeffd0ee8231be6b5065a7a
Josefine Isberg <95719327+sojse@users.noreply.github.com>
2022-11-10 12:53:18 +0100

    Refactor AJAX call

--- script.js
+++ script.js
@@ -17,71 +17,50 @@ function init () {
  * output are called. If the user presses the button multiple times the previous temperature icon will 
  * disapear
  */
-function clickWeatherButton() {
+async function clickWeatherButton() {
     document.getElementById('low-temp').style.display = 'none';
     document.getElementById('high-temp').style.display = 'none';
     document.getElementById('neutral-temp').style.display = 'none';
 
+
     let city = document.getElementById('city').value;
-    let coordinates = getCoordinates(city);
-    let weatherInfo = getWeather(coordinates);
+    let coordinates = await getCoordinates(city);
+    let weatherInfo = await getWeather(coordinates);
     displayTemperature(weatherInfo);
-    displayCity(city);
+    displayCity(city);    
 }
 
 /**
- * Making a AJAX request to get the location data from the API
+ * Making a AJAX request to fetch the location data from the API
  * Returns an object with longitude and lattitude
  */
-function getCoordinates(city) {
-    //Ajax object constructor that will be used to construct a new HTTP request
-    let request = new XMLHttpRequest();
+async function getCoordinates (city) {
     let coordinates = {};
+    await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=8e741e5447944515dd0d2a32ea8269a0`)
+    .then((response) => response.json())
+    .then((data) => {
+        coordinates.latitude = data[0].lat;
+        coordinates.longitude = data[0].lon;
+    });
 
-    // False means that the request will be synchronous, meaning the data is fully retrieved before the rest of the code runs
-    request.open('GET', `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=8e741e5447944515dd0d2a32ea8269a0`, false);
-
-    //To make the data available to us I add an eventlistener that waits for the data to load
-    request.addEventListener('load', () => {
-        //Checks if the request was successfull and if it is ready
-        if (request.status === 200 && request.readyState === 4) {
-            let result = JSON.parse(request.responseText);
-            coordinates.lattitude = result[0].lat;
-            coordinates.longitude = result[0].lon;
-        } else {
-            throw new Error('Bad request');
-        }
-    })
-
-    request.send();
     return coordinates;
 }
 
 /**
- * Making an AJAX call to get the weather data by using the coordinates from getCoordinates
+ * Making an AJAX call to fetch the weather data by using the coordinates from getCoordinates
  * The unit used for degrees will be Celsius 
  */
-function getWeather(coordinates) {
-    let request = new XMLHttpRequest();
+async function getWeather (coordinates) {
     let weatherInfo = {};
+    await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.latitude}&lon=${coordinates.longitude}&units=metric&appid=8e741e5447944515dd0d2a32ea8269a0`)
+    .then((response) => response.json())
+    .then((data) => {
+        weatherInfo.weatherDescription = data.weather[0].description;
+        weatherInfo.temperature = data.main.temp;
+    });
 
-    request.open('GET', `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lattitude}&lon=${coordinates.longitude}&units=metric&appid=8e741e5447944515dd0d2a32ea8269a0`, false);
-
-    request.addEventListener('load', () => {
-        //Checks if the request was successfull and if it is ready
-        if (request.status === 200 && request.readyState === 4) {
-            let result = JSON.parse(request.responseText);
-            weatherInfo.weatherDescription = result.weather[0].description;
-            weatherInfo.temperature = result.main.temp;
-        } else {
-            throw new Error('Bad request');
-        }
-    })
-
-    request.send();
     return weatherInfo;
 }
-
 
 /**
  * Displays the weather information for the user and changes the background and termometer
@@ -97,12 +76,13 @@ function displayTemperature (weather) {
     if (temperature >= 15) {
         body.className = 'background-warm';
         document.getElementById('high-temp').style.display = 'block';
-    } else if (temperature <=14 && temperature >= 1) {
+    } else if (temperature <= 0) {
+        body.className = 'background-cold';
+        document.getElementById('low-temp').style.display = 'block';        
+    } else {
         body.className = 'background-neutral';
         document.getElementById('neutral-temp').style.display = 'block';
-    } else {
-        body.className = 'background-cold';
-        document.getElementById('low-temp').style.display = 'block';
+
     }
 }
 
